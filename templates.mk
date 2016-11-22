404d5f7d18ab4f91c85b19c4db0a2a699ed3faee
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-11-22 16:13:41 +0100

    Add Jenkins

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs puppet upkick
+TEMPLATES = conplicity gogs jenkins puppet upkick
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
