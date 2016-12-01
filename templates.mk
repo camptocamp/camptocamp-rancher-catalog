fd5ef10004a4e0d4006d0ef841f867a23e3109c9
Marc Sutter <marc.sutter@swisscom.com>
2016-12-01 13:53:27 +0100

    add mopper

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs jenkins puppet upkick
+TEMPLATES = conplicity gogs jenkins puppet upkick mopper
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
