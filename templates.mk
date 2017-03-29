3417723712ddcd03c9e4c757ca5188d1f6bf75ee
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-03-29 13:25:25 +0200

    Add minio

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs jenkins puppet upkick mopper s3fs
+TEMPLATES = conplicity gogs jenkins puppet upkick minio mopper s3fs
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
