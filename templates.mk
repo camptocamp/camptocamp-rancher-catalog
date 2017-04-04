563b5e06388c1f8a1115edae25e0485bb7065793
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-04-04 10:27:06 +0200

    Add goofys

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs jenkins puppet upkick minio mopper s3fs s3proxy
+TEMPLATES = conplicity gogs goofys jenkins puppet upkick minio mopper s3fs s3proxy
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
