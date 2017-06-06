37bebba4a105e6ded4e5763b964a2767e5a52651
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-06-06 16:41:57 +0200

    Add ipsec-status

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs goofys jenkins puppet upkick minio mopper s3fs s3proxy
+TEMPLATES = conplicity gogs goofys ipsec-status jenkins puppet upkick minio mopper s3fs s3proxy
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
