bc6b2a532b0c045752958ada3ffbd636f1592e58
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-09-28 15:10:40 +0200

    Add gitea for catalog

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gogs goofys ipsec-status jenkins puppet upkick minio mopper s3fs s3proxy terraboard
+TEMPLATES = conplicity gitea gogs goofys ipsec-status jenkins puppet upkick minio mopper s3fs s3proxy terraboard
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
