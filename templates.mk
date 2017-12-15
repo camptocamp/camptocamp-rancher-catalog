b7bedf30ef917e606ee812de6de46e1f3759b564
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-12-15 11:09:52 +0100

    OpenSource metrics and logging templates

--- templates.mk
+++ templates.mk
@@ -1,4 +1,4 @@
-TEMPLATES = conplicity gitea gogs goofys ipsec-status jenkins postgres-cluster puppet upkick minio mopper s3fs s3proxy terraboard
+TEMPLATES = conplicity gitea gogs goofys ipsec-status jenkins postgres-cluster puppet upkick minio mopper s3fs s3proxy terraboard metrics logging
 GIT_URL_BASE = git@github.com:camptocamp/rancher-template-
 REMOTE_BASE = gh-rancher-template-
 
